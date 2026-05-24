6537fcc0a56a6b6ce975716302b051b972014f67
r <146114263+xngxpta@users.noreply.github.com>
2026-05-25 00:27:02 +0530

    Update 3 - AnalyticsBox.py

--- pages/3 - AnalyticsBox.py
+++ pages/3 - AnalyticsBox.py
@@ -1204,12 +1204,12 @@ with st.container(border=True):
         fig_macd.update_xaxes(visible=True, showticklabels=True)
         fig_macd.update_yaxes(title='MACD Signal',
                               visible=True, showticklabels=True)
-        fig_macd = px.bar(
+        fig_macd_signal= px.bar(
             df["trend_macd_signal"])
-        fig_macd.update_layout(
+        fig_macd_signal.update_layout(
             title='MACD 9 Day', showlegend=True)
-        fig_macd.update_xaxes(visible=True, showticklabels=True)
-        fig_macd.update_yaxes(
+        fig_macd_signal.update_xaxes(visible=True, showticklabels=True)
+        fig_macd_signal.update_yaxes(
             title='MACD Signal', visible=True, showticklabels=True)
 
         fig_macd_diff = px.area(df["trend_macd_diff"])
