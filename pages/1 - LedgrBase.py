839c065b61c55a7679af6c231ba7e8cf9649efc9
r <146114263+xngxpta@users.noreply.github.com>
2026-07-17 15:03:20 +0530

    Integrate Google Analytics tracking
    Added Google Analytics tracking code to the Streamlit app.

--- pages/1 - LedgrBase.py
+++ pages/1 - LedgrBase.py
@@ -21,9 +21,21 @@ from plotly.subplots import make_subplots
 from selectolax.parser import HTMLParser
 import requests
 import streamlit as st
-# from auth.session import init_session
-#from st_paywall import add_auth
-# init_session()
+import streamlit.components.v1 as components
+
+GA_ID = "G-4MRDCEFGB4"
+
+gtag_html = f"""
+<script async src="https://www.googletagmanager.com/gtag/js?id={GA_ID}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){{dataLayer.push(arguments);}}
+  gtag('js', new Date());
+  gtag('config', '{GA_ID}');
+</script>
+"""
+# Embed the script
+components.html(gtag_html, height=0)
 
 st.set_page_config(
     page_title="LedgrBase | Your Asset Dossier",
