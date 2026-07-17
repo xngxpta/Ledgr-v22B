1e3082c4cd051084e06f2239783b5ce295962bf7
r <146114263+xngxpta@users.noreply.github.com>
2026-07-17 15:15:18 +0530

    Remove Google Analytics integration
    Removed Google Analytics tracking script from the page.

--- pages/6 - Resources_and_Tutorials.py
+++ pages/6 - Resources_and_Tutorials.py
@@ -5,21 +5,7 @@ import base64
 import os
 import urllib
 from streamlit_pdf_viewer import pdf_viewer
-import streamlit.components.v1 as components
 
-GA_ID = "G-4MRDCEFGB4"
-
-gtag_html = f"""
-<script async src="https://www.googletagmanager.com/gtag/js?id={GA_ID}"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){{dataLayer.push(arguments);}}
-  gtag('js', new Date());
-  gtag('config', '{GA_ID}');
-</script>
-"""
-# Embed the script
-components.html(gtag_html, height=0)
 
 st.set_page_config(page_title='Ledgr | About & Tutorials', layout="wide",
                    initial_sidebar_state="expanded")
